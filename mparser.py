86dc68a03b37735055392e9879262231ab816b33
Kirill Maslinsky <kirill@altlinux.org>
2011-09-02 12:05:35 +0400

    workaround for tokenization with unicode accents
    hardcoded acute and grave combining marks into word
    token regexp to avoid splitting words on diacritic marks

--- mparser.py
+++ mparser.py
@@ -39,7 +39,8 @@ class Tokenizer(object):
                 ('Punct', (r'([:;,]+)',re.UNICODE)),
                 ('SentPunct', (r'([.!?]+|[)"])',re.UNICODE)),
                 ('Cardinal', (r'\d+',re.UNICODE)),
-                ('Word', (r"\w+([-]\w+)*[']?",re.UNICODE)),
+                #FIXME: hardcoded acute and grave accents (shoud not split words)
+                ('Word', (ur"(\w[\u0300\u0301]?)+([-](\w[\u0300\u0301]?)+)*[']?",re.UNICODE)),
                 ('Nonword', (r'\W', re.UNICODE)),
                 ]
         useless = ['NL', 'Space']
