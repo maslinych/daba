9e23cbcd4e2bd292e722255698b96883e7cc82ae
Kirill Maslinsky <kirill@altlinux.org>
2012-10-15 19:20:39 +0400

    metaprint: add --all option

--- metaprint.py
+++ metaprint.py
@@ -13,17 +13,27 @@ def main():
     aparser.add_argument('-w', '--words', dest='fields', action='append_const', const='_auto:words', help='Number of word tokens in document')
     aparser.add_argument('-s', '--sentences', dest='fields', action='append_const', const='_auto:sentences', help='Number of sentences in document')
     aparser.add_argument('-p', '--paragraphs', dest='fields', action='append_const', const='_auto:paragraphs', help='Number of paragraphs in document')
-    aparser.add_argument('-d', '--delimeter', action='store', default="\t")
+    aparser.add_argument('-d', '--delimeter', action='store', default="\t", help='Delimiter for output fields')
+    aparser.add_argument('-a', '--all', action='store_true', help='Print all metadata found in a file')
     args = aparser.parse_args()
 
     reader = formats.HtmlReader(args.infile, onlymeta=True)
     meta = defaultdict(unicode)
     for k,v in reader.metadata:
         meta[k] = v
+
     sys.stdout.write(unicode(args.infile).encode('utf-8'))
-    sys.stdout.write(args.delimeter)
-    sys.stdout.write(args.delimeter.join([meta[field] for field in args.fields]).encode('utf-8'))
-    sys.stdout.write('\n')
+
+    if args.all:
+        sys.stdout.write('\n')
+        for name in sorted(meta.iterkeys()):
+            sys.stdout.write(u"\t".join([name, meta[name]]).encode('utf-8'))
+            sys.stdout.write('\n')
+        sys.stdout.write('\n\n')
+    else:
+        sys.stdout.write(args.delimeter)
+        sys.stdout.write(args.delimeter.join([meta[field] for field in args.fields]).encode('utf-8'))
+        sys.stdout.write('\n')
 
 if __name__ == '__main__':
     main()
