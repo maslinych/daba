7ef544af2ae7e77b8f2fcea52c9f1fb13977d0e7
vieenrose <luigi.liu@laposte.net>
2017-04-18 19:08:40 +0200

    Reduction of effective code length

--- differential_tone_coding.py
+++ differential_tone_coding.py
@@ -5,6 +5,11 @@ import unicodedata
 import sys
 import Levenshtein
 
+markers_tone =  [unichr(0x0300),unichr(0x0301),unichr(0x0302),unichr(0x030c)]
+markers_pause = [unichr(0x002e)]
+PREDICTION_GENERAL = True
+PREDICTION_TONES_PAUSE = True
+
 def differential_encode (form_non_tonal, form_tonal) :
 
 	form_non_tonal = unicodedata.normalize('NFD', form_non_tonal)
@@ -12,27 +17,27 @@ def differential_encode (form_non_tonal, form_tonal) :
 	ops = Levenshtein.editops(form_non_tonal, form_tonal)
 
 	ret = ""
-	# dl = False
+	if PREDICTION_TONES_PAUSE :
+		markers = markers_tone + markers_pause
+	else : # PREDITION_TONES_ONLY
+		markers = markers_tone
 	for op in ops :
 
 		mode, p_src, p_dst = op
-		ret += mode[0] + str(p_src)
-
-		if mode == 'delete' :
-			ret += form_non_tonal[p_src]
-			#	dl = True
-		else :
-			ret += form_tonal[p_dst]
-	#if dl:
-	#	print ret, form_non_tonal,form_tonal
+		# codage des marqueurs de ton
+		if mode == "delete" :
+			if form_non_tonal[p_src] in markers or PREDICTION_GENERAL :
+				ret += 'd' + str(p_src) + form_non_tonal[p_src]
+		else : # mode == "insert" or "replace"
+			if form_tonal[p_dst] in markers or PREDICTION_GENERAL :
+				if mode == "replace" : ret += 'r'
+				ret += str(p_src) + form_tonal[p_dst]
 	return ret
 
 def main () :
 
-	form_non_tonal = u"dɔnnin"
-	form_tonal     = u"dɔ́nni"
-	form_non_tonal = u'malidenwx'
-	form_tonal = u'Màlidenw'
+	form_non_tonal = u'abécèi'
+	form_tonal     = u'àbècel'
 
 	print form_non_tonal
 	print form_tonal
