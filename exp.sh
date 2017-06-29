b82b4db95511e0be2b461cb0a584ace294926e36
vieenrose <luigi.liu@laposte.net>
2017-06-29 16:54:40 +0200

    Test script update

--- exp.sh
+++ exp.sh
@@ -3,7 +3,7 @@
 #set -vx
 
 GIT_VERSION="$(git rev-parse HEAD)"
-NOM=exp_$(date +%H_%M_%S)_"$GIT_VERSION"
+NOM=exp_$(date +%H_%M)_"$GIT_VERSION"
 
 BASIC_OPTIONS="-v -t -l $NOM.mod"
 SUPP_OPTIONS="-e 1 --filtering"
@@ -12,17 +12,22 @@ KEYWORD="Seconds required for this iteration: |Error norm|Iteration #"
 KEYWORD2="diacritic_only|chunkmode|filtering|no_coding|no_decomposition|r_E|accuracy|done"
 FP_PAT="[-+]?[0-9]+\.?[0-9]*"
 
+touch "$NOM.log"
+
 for d in "--diacritic_only" "--diacritic_only --no_decomposition" "" "--no_decomposition"
 do
 for w in -1 1 2 3 4 5 6 7 0
 do
 VAR_OPTS="-c $w $d"
 
-gstdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
-| gawk "BEGIN{IGNORECASE=1} /.*($KEYWORD2).*/ {print \$0} match(\$0, /.*($KEYWORD)[^.0-9+-]*($FP_PAT)/, ary) {print ary[2]}" \
->> "$NOM.log" \ || \
+if hash stdbuf 2>/dev/null; then
 stdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
 | gawk "BEGIN{IGNORECASE=1} /.*($KEYWORD2).*/ {print \$0} match(\$0, /.*($KEYWORD)[^.0-9+-]*($FP_PAT)/, ary) {print ary[2]}" \
 >> "$NOM.log"
+else
+gstdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
+| gawk "BEGIN{IGNORECASE=1} /.*($KEYWORD2).*/ {print \$0} match(\$0, /.*($KEYWORD)[^.0-9+-]*($FP_PAT)/, ary) {print ary[2]}" \
+>> "$NOM.log"
+fi
 done
 done
