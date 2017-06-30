2e3adaa0a6eb07d5eefaa4803f8566d0598e545f
vieenrose <luigi.liu@laposte.net>
2017-07-01 00:51:08 +0200

    split exp.sh in 4 files

--- exp_accuracy_vs_segmentation_type_no_decomposition_B.sh
+++ exp_accuracy_vs_segmentation_type_no_decomposition_B.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-#set -vx
+set -vx
 
 GIT_VERSION="$(git rev-parse HEAD)"
-NOM=exp_$(date +%H_%M)_"$GIT_VERSION"
+NOM=exp_accuracy_vs_segmentation_type_no_decomposition_B_$(date +%d_%H_%M)_"$GIT_VERSION"
 
-BASIC_OPTIONS="-v -t -l $NOM.mod"
-SUPP_OPTIONS="-e 10 --filtering"
+BASIC_OPTIONS="-v -t -l $NOM"
+SUPP_OPTIONS="-e 1 --filtering --diacritic_only --no_decomposition"
 
 KEYWORD="Seconds required for this iteration: |Error norm|Iteration #"
 KEYWORD2="[^_]diacritic_only|chunkmode|filtering|no_coding|no_decomposition|r_E|accuracy|done|eval|total"
@@ -14,11 +14,9 @@ FP_PAT="[-+]?[0-9]+\.?[0-9]*"
 
 touch "$NOM.log"
 
-for d in "--diacritic_only" "--diacritic_only --no_decomposition" "" "--no_decomposition"
+for w in 4 5 6 0
 do
-for w in -1 1 2 3 4 5 6 7 0
-do
-VAR_OPTS="-c $w $d"
+VAR_OPTS="-c $w -s "$NOM"_w_"$w".csv"
 
 if hash stdbuf 2>/dev/null; then
 stdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
@@ -29,5 +27,4 @@ gstdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
 | gawk "BEGIN{IGNORECASE=1} /.*($KEYWORD2).*/ {print \$0} match(\$0, /.*($KEYWORD)[^.0-9+-]*($FP_PAT)/, ary) {print ary[2]}" \
 >> "$NOM.log"
 fi
-done
 done
---
+++ exp_accuracy_vs_trainsize.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -vx
+
+GIT_VERSION="$(git rev-parse HEAD)"
+NOM=exp_accuracy_vs_trainsize_$(date +%d_%H_%M)_"$GIT_VERSION"
+
+BASIC_OPTIONS="-v -t -l $NOM"
+SUPP_OPTIONS="--filtering --diacritic_only"
+
+KEYWORD="Seconds required for this iteration: |Error norm|Iteration #"
+KEYWORD2="[^_]diacritic_only|chunkmode|filtering|no_coding|no_decomposition|r_E|accuracy|done|eval|total"
+FP_PAT="[-+]?[0-9]+\.?[0-9]*"
+
+touch "$NOM.log"
+
+for trainsize in 1 10 20 30 40 50 60 70 80 90
+do
+VAR_OPTS="-e $trainsize -s "$NOM_trainsize"_"$trainsize".csv"
+if hash stdbuf 2>/dev/null; then
+stdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
+| gawk "BEGIN{IGNORECASE=1} /.*($KEYWORD2).*/ {print \$0} match(\$0, /.*($KEYWORD)[^.0-9+-]*($FP_PAT)/, ary) {print ary[2]}" \
+>> "$NOM.log"
+else
+gstdbuf -oL python disambiguation.py $VAR_OPTS $SUPP_OPTIONS $BASIC_OPTIONS \
+| gawk "BEGIN{IGNORECASE=1} /.*($KEYWORD2).*/ {print \$0} match(\$0, /.*($KEYWORD)[^.0-9+-]*($FP_PAT)/, ary) {print ary[2]}" \
+>> "$NOM.log"
+fi
+done
