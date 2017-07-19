1ee2e83d773332fd83f0b54df236e42394bb19dc
vieenrose <luigi.liu@laposte.net>
2017-07-19 16:55:04 +0200

    Revert "Un lecteur de texte brut transmet le texte qu'il reçoit en entrée en list(list([token_non_marked, token_marked]))"
    This reverts commit e3c523e4bd42caac15458dec715abac20cd9716d.

--- fileReader.py
+++ fileReader.py
@@ -3,76 +3,64 @@
 
 import unicodedata,argparse,codecs,sys,re
 
-class fileReader () :
+def get_cat_startwith(str_in) :
 
-	def __init__(self, customed_markers = "") :
-		self.strp = \
-			self.__get_cat_startwith('Zl') + \
-			self.__get_cat_startwith('Zp') + \
-			self.__get_cat_startwith('Zs') + '\n'
-	        sep_sent  = \
-			self.__get_cat_startwith('Pi') + \
-			self.__get_cat_startwith('Pf') + \
-			self.__get_cat_startwith('Po')
-        	sep_token = \
-			self.__get_cat_startwith('Zs')
+	return u"".join([unichr(i) for i in xrange(sys.maxunicode) if unicodedata.category(unichr(i)).startswith(str_in)])
 
-		pat1   = u'([{}]+)'.format(sep_sent)
-		pat2   = u'[{}]+'.format(sep_token)
-		self.regex1 = re.compile(pat1, flags = re.IGNORECASE)
-		self.regex2 = re.compile(pat2, flags = re.IGNORECASE)
+def file_to_sentences(filein) :
 
-		if not customed_markers :
-			self.markers = self.__get_cat_startwith('Mn')
-		else :
-			self.markers = customed_markers
+	# regex for spliting / tokenization
+	strp = get_cat_startwith('Zl') + \
+	       get_cat_startwith('Zp') + \
+     	       get_cat_startwith('Zs')
+	sep_token  = get_cat_startwith('Zs')
+	sep_sent = get_cat_startwith('Pi') + \
+	           get_cat_startwith('Pf') + get_cat_startwith('Po')
+	pat1 = u'([{}]+)'.format(sep_sent)
+	pat2 = u'[{}]+'.format(sep_token)
+	regex1 = re.compile(pat1, flags=re.IGNORECASE)
+	regex2 = re.compile(pat2, flags=re.IGNORECASE)
 
-	def read(self, filein) :
-        	sentences = list()
+	# routine de lecture
+	sentences = list()
+	try :
 		with codecs.open(filein, 'r', 'utf-8') as file:
 			for line in file :
-				para = line.strip(self.strp)
-				sents = self.regex1.split(para)
+				para = line.strip(strp)
+				sents = regex1.split(para)
 				for sent in sents :
-					tokens = self.regex2.split(sent)
+					tokens = regex2.split(sent)
 					sentence = list()
 					for token in tokens :
 						if token :
-							sentence.append([self.__mask(token), self.__unicode_decomp(token)])
+							# [prediction source, prediction destination]
+							sentence.append([mask(token), token])
 					if sentence :
 						sentences.append(sentence)
 		return sentences
+	except :
+		return []
 
-	def __unicode_decomp(self, str_in) :
-		return unicodedata.normalize('NFD', str_in)
+def mask(str_in, sym_to_mask = get_cat_startwith('Mn')) :
+	return u"".join([c for c in str_in if c not in sym_to_mask])
 
-	def __mask(self, str_in) :
-		str_in = self.__unicode_decomp(str_in)
-		return u"".join([c for c in str_in if c not in self.markers])
+if __name__ == "__main__" :
 
-	def __get_cat_startwith(self, str_in) :
-		return u"".join([unichr(i) for i in xrange(sys.maxunicode) \
-			if unicodedata.category(unichr(i)).startswith(str_in)])
+	# interface en ligne de commande
+	#parser = argparse.ArgumentParser()
+	#parser.add_argument('file')
+	#args = parser.parse_args()
+	file = "../Tashkeela-arabic-diacritized-text-utf8-0.3/texts.txt/تحفة المحتاج في شرح المنهاج.txt"
+	file = "../Tashkeela-arabic-diacritized-text-utf8-0.3/texts.txt/فضائل مجالس الذكر .txt"
 
-def view_allsents(sentences) :
+	#
+	sentences = file_to_sentences(file)
 	s_num = 0
 	for sent in sentences :
 		if sent :
 			t_num = 0
 			for token in sent :
-				if token :
-					print u"{:6d}{:3d}: '{}' -> '{}'".format(s_num,t_num,token[0],token[1])
+				if token[1] :
+					print u"{:6d}{:3d}: {} -> {}".format(s_num,t_num,token[0],token[1])
 					t_num += 1
 			s_num += 1
-			print
-
-if __name__ == "__main__" :
-
-	# interface en ligne de commande
-	parser = argparse.ArgumentParser()
-	parser.add_argument('file')
-	args = parser.parse_args()
-
-	fr = fileReader()
-	sentences = fr.read(args.file)
-	view_allsents(sentences)
