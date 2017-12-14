5e823b426cf3dc02a3aa4663126bf84904d5db39
Kirill Maslinsky <kirill@altlinux.org>
2017-12-14 15:00:08 +0100

    update package dependencies

--- setup.py
+++ setup.py
@@ -87,8 +87,15 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['funcparserlib', 'intervaltree', 'pytrie', 'wxPython'],
-    
+    install_requires=[
+        'funcparserlib',
+        'intervaltree',
+        'pytrie',
+        'wxPython',
+        'Levenshtein',
+        'nltk',
+        'pycrfsuite'
+    ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
